7a48bcb78263d14b8891358ae8caa1f4df966e3e
K4r0lP <kpastuszka@student.agh.edu.pl>
2021-11-18 11:29:52 +0100

    cw4

--- cw5/cw5.sql
+++ cw5/cw5.sql
@@ -1,32 +1,19 @@
 drop table obiekty;
 --tworze tabele
 
-create table obiekty(id INT primary key,name varchar(255),geometry GEOMETRY);
+create table obiekty(id INT primary key,geometry GEOMETRY,name varchar not null);
 
 --dodawanie obiektów
 
---obiekt 1
-insert into obiekty(id,name,geometry) values(
-	1,'obiekt1',ST_GeomFromText('MULTICURVE(LINESTRING(0 1, 1 1),CIRCULARSTRING(1 1, 2 0, 3 1),
-								 CIRCULARSTRING(3 1, 4 2, 5 1),LINESTRING(5 1, 6 1))',-1));
-								 
---obiekt 2
-insert into obiekty(id,name,geometry) values(
-	2,'obiekt2',ST_GeomFromText('CURVEPOLYGON(COMPOUNDCURVE(LINESTRING(10 6, 14 6),CIRCULARSTRING(14 6, 16 4, 14 2),
- CIRCULARSTRING(14 2, 12 0, 10 2),LINESTRING(10 2, 10 6)),CIRCULARSTRING(11 2, 13 2, 11 2))',-1));
+insert into obiekty values
+(1, st_geomFromText('compoundcurve( (0 1, 1 1), circularstring(1 1, 2 0, 3 1), circularstring(3 1, 4 2, 5 1), (5 1, 6 1) )'), 'obiekt1'),
+(2, st_geomFromText('curvepolygon(compoundcurve( (10 6, 14 6), circularstring(14 6, 16 4, 14 2),
+			 circularstring(14 2, 12 0, 10 2), (10 2, 10 6)), circularstring(11 2, 12 3, 13 2, 12 1, 11 2))'), 'obiekt2'),
+(3, st_geomFromText('multicurve( (7 15, 10 17), (10 17, 12 13), (12 13, 7 15) )' ), 'obiekt3'),
+(4, st_geomFromText('multicurve((20 20, 25 25), (25 25, 27 24), (27 24, 25 22), (25 22, 26 21), (26 21, 22 19), (22 19, 20.5 19.5))'), 'obiekt4'),
+(5, st_geomFromText('multipoint(30 30 59, 38 32 234)'), 'obiekt5'),
+(6, st_geomFromText('geometrycollection(point(4 2), linestring(1 1, 3 2))'), 'obiekt6');
 
---obiekt 3							
-insert into obiekty(id,name,geometry) values(3,'obiekt3',ST_GeomFromText('POLYGON((10 17, 12 13, 7 15, 10 17))',-1));
-
---obiekt 4	
-insert into obiekty(id,name,geometry) values(
-	4,'obiekt4',ST_GeomFromText('MULTILINESTRING((20 20, 25 25),(25 25, 27 24),(27 24, 25 22),(25 22, 26 21),(26 21, 22 19),(22 19, 20.5 19.5))',-1));
-
---obiekt 5	
-insert into obiekty(id,name,geometry) values(5,'obiekt5',ST_GeomFromText('MULTIPOINT((30 30 59),(38 32 234))',-1));
-	
---obiekt 6	
-insert into obiekty(id,name,geometry) values(6,'obiekt6',ST_GeomFromText('GEOMETRYCOLLECTION(LINESTRING(1 1, 3 2),POINT(4 2))',-1));
 
 --zapytania
 --1
@@ -36,17 +23,33 @@ select ST_Area(ST_Buffer(ST_ShortestLine(a.geometry,b.geometry),5))
 		
 --2
 update obiekty
-	set geometry = ST_GeomFromText('POLYGON((20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5, 20 20))',-1)
+	set geometry = ST_GeomFromText('polygon((20 20, 25 25, 27 24, 25 22, 26 21, 22 19, 20.5 19.5, 20 20))',-1)
 		where name = 'obiekt4';
+
+
+
+
+
+
+
+
+
 
 --3
 insert into obiekty(id,name,geometry) values(
-	7,
-	'obiekt7',
-	(select ST_Union(a.geometry, b.geometry) from obiekty a, obiekty b
+	8,
+	'obiekt8',
+	--select St_Collect(a.geometry, b.geometry)
+	(select St_Collect(a.geometry, b.geometry) from obiekty a, obiekty b
 		where a.name = 'obiekt3' and b.name = 'obiekt4')
 	);
-	
+
+select st_asText(geometry) from obiekty 
+where name = 'obiekt8' or  name = 'obiekt7'
+
+
+
+
 --4
 select name,ST_Area(ST_Buffer(geometry,5))
 	from obiekty
